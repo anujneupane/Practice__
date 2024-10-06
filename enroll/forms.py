fac08d0d166325c4320293d4b2c524a1c19a6544
anujneupane <anujneupane987@gmail.com>
2024-10-06 21:41:14 +0545

    solve issue

--- enroll/forms.py
+++ enroll/forms.py
@@ -8,14 +8,13 @@ class FormValid(forms.Form):
      rpassword = forms.CharField(label='Password(Retype)',widget=forms.PasswordInput)
 
 
-def clean(self):
-    cleaned_data = super().clean()
-    pwd = self.cleaned_data['password']
-    rpwd = self.cleaned_data['rpassword']
-    if pwd != rpwd:  
-      raise forms.ValidationError('Passwords do not match')
-    return cleaned_data
-
+     def clean(self):
+       cleaned_data = super().clean()
+       pwd = self.cleaned_data['password']
+       rpwd = self.cleaned_data['rpassword']
+       if pwd != rpwd:  
+        raise forms.ValidationError('Passwords do not match')
+       return cleaned_data
  
         
        
